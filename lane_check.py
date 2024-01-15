eeeea17f721ce4691dfc99d7f1698837fd07950c
amrmsab <46768079+amrmsab@users.noreply.github.com>
2024-01-15 13:29:40 -0500

    added checks

---
+++ lane_check.py
@@ -0,0 +1,13 @@
+def test_braitenberg_submission():
+    try:
+        with open('lane-following/submission.md', 'r', encoding='utf-8') as file:
+            submission_no = file.readline().strip()
+            user_no = file.readline().strip()
+    except FileNotFoundError:
+        print(f"File not found")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    assert submission_no.isdigit(), "Input must contain only numerical digits"
+    assert len(submission_no) == 5, "Input must contain exactly 5 digits"
+    assert len(user_no) > 1, "User name is not provided"
