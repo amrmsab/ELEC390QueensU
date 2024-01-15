54c0813b84b16e1b51af6edd7d960ec28fbefc5a
amrmsab <46768079+amrmsab@users.noreply.github.com>
2024-01-15 12:43:25 -0500

    Create checks_test2.py

---
+++ checks_test2.py
@@ -0,0 +1,10 @@
+def test1():
+  try:
+    with open('braitenberg/submission.md', 'r', encoding='utf-8') as file:
+      submission_no = file.readline().strip()
+  except FileNotFoundError:
+    print(f"File not found: {file_path}")
+  except Exception as e:
+    print(f"An error occurred: {e}")
+  assert submission_no.isdigit(), "Input must contain only numerical digits"
+  assert len(submission_no) == 5, "Input must contain exactly 5 digits"
